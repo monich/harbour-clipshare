6f6020f8e218d4965b84d3b1923813f24f78a275
Åke Engelbrektson <eson57@users.noreply.github.com>
2026-09-07 08:29:35 +0200

    Create harbour-clipshare-sv.ts
    Add Swedish translation

---
+++ translations/harbour-clipshare-sv.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="clipshare-info-clipboard_empty">
+        <source>No text in clipboard</source>
+        <extracomment>Info label</extracomment>
+        <translation>Ingen text i urklipp</translation>
+    </message>
+    <message id="clipshare-button-share">
+        <source>Share</source>
+        <extracomment>Button label</extracomment>
+        <translation>Dela</translation>
+    </message>
+    <message id="clipshare-title-share_text">
+        <source>Share text</source>
+        <extracomment>Sharing page header</extracomment>
+        <translation>Dela text</translation>
+    </message>
+    <message id="clipshare-button-add_account">
+        <source>Add account</source>
+        <extracomment>Sharing method list item</extracomment>
+        <translation>Lägg till konto</translation>
+    </message>
+</context>
+</TS>
